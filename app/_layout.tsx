5fffbd1b8fe66f2120c0d2e8a1fe4a8ad7a81921
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2026-06-28 13:07:45 +0600

    added css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,57 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
+const properties = [
+  { id: "1", name: "Property 1" },
+  { id: "2", name: "Property 2" },
+  { id: "3", name: "Property 3" },
+];
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView className="bg-white p-10">
+      <View>
+        <Text>Welcome to the app!</Text>
+        <TextInput placeholder="Search City" placeholderTextColor="gray" />
+
+        <TouchableOpacity
+          onPress={() => {
+            console.log("Button pressed!");
+          }}
+          style={{
+            backgroundColor: "blue",
+            padding: 12,
+            borderRadius: 5,
+            marginTop: 10,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>Click me!</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={properties}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ padding: 16 }}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              backgroundColor: "#f0f0f0",
+              padding: 16,
+              borderRadius: 8,
+              marginBottom: 12,
+            }}
+          >
+            <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+            <Text style={{ color: "blue" }}>Property ID: {item.id}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import "../global.css"
+export default function Index() {
+  return (
+    <View>
+      <Text>Welc   ome to the app!</Text>
+    </View>
+  );
+}
